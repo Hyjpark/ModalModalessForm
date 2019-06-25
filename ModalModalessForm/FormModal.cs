f5f32147bfa4791d6d77603f55ab5a557e4396f5
Hyjpark <hyej042@gmail.com>
2019-06-25 15:12:19 +0900

    #2 Modal & Modaless 창 띄우기 코드 구현 by 3207 박혜정

---
+++ ModalModalessForm/FormModal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ModalModalessForm
+{
+    public partial class FormModal : Form
+    {
+        public string Title
+        {
+            set { this.Text = value; }
+        }
+        public FormModal()
+        {
+            InitializeComponent();
+        }
+
+        private void FormModal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
